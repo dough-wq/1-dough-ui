4227d243d43af1a4283948d126c85e079d77a8a8
doughs <1572569345@qq.com>
2023-03-10 16:17:47 +0800

    chore: 将测试工具由jest改为vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite';
-import type { PluginOption } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import VueSetupExtend from 'vite-plugin-vue-setup-extend';
-import StylelintPlugin from 'vite-plugin-stylelint';
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import type { PluginOption } from "vite";
+import vue from "@vitejs/plugin-vue";
+import VueSetupExtend from "vite-plugin-vue-setup-extend";
+import StylelintPlugin from "vite-plugin-stylelint";
 
 export default defineConfig({
-  plugins: [StylelintPlugin({fix: true}),vue() as PluginOption, VueSetupExtend()],
+  plugins: [
+    StylelintPlugin({ fix: true }),
+    vue() as PluginOption,
+    VueSetupExtend(),
+  ],
   server: {
-    host:'0.0.0.0'
-  }
+    host: "0.0.0.0",
+  },
+  test: {
+    environment: "jsdom",
+  },
 });
-
